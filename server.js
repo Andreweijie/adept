f7d34d91186fde12c56821f78eca5d6fef6317c8
Andre Goh <andregoh1996@gmail.com>
2019-09-16 19:22:25 +0800

    update 16/9

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 const cors = require("cors");
 const app = express();
 const authRoutes = require("./routes/auth");
@@ -20,6 +21,9 @@ app.use("/backend/api", authRoutes);
 app.use("/backend/cust", custRoutes);
 app.use("/backend/admin", adminRoutes);
 
+app.get("/test", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/html/account-confirmed.html"));
+});
 mongoose
   .connect(
     "mongodb+srv://andreweijie:1234@users-aeexa.mongodb.net/test?retryWrites=true",
